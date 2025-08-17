52b35550e974bb9a4de0f97bdf46a5bac7cea24c
Anirudh Sriram <hello@techwithanirudh.com>
2025-08-17 20:53:51 +0000

    refactor: remove post-create command and delete configure-p10k script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,52 @@
+import { fileURLToPath } from 'node:url'
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+import createJiti from 'jiti'
 import type { NextConfig } from 'next'
 
+const jiti = createJiti(fileURLToPath(import.meta.url))
+jiti('./src/env')
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: process.env.SOURCE_MAPS === 'true',
+  devIndicators: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: "frame-ancestors 'self';",
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+      ],
+    },
+  ],
 }
 
-export default nextConfig
+const bundleAnalyzerPlugin = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const NextApp = () => {
+  const plugins = [bundleAnalyzerPlugin]
+  return plugins.reduce((config, plugin) => plugin(config), nextConfig)
+}
+
+export default NextApp
