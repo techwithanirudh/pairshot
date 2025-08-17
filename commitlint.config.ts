8233a10e2d8188ea3e0b1b1c6a962575559202d8
Anirudh Sriram <hello@techwithanirudh.com>
2025-08-17 19:21:42 +0000

    refactor: simplify commitlint configuration by removing unused parserPreset and formatter

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,8 +2,6 @@ import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format',
 };
 
 export default config;
